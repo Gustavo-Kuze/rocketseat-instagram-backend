7b32c58e077edafbc7b6c54a77245a896faaa61b
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-06-12 11:27:02 -0300

    Redimensionamento de imagens com Jimp (ao invés do Sharp)

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,4 +1,7 @@
 const Post = require('../models/Post');
+const jimp = require('jimp')
+const path = require('path')
+const fs = require('fs')
 
 module.exports = {
     async index(req, res) {
@@ -7,12 +10,27 @@ module.exports = {
     },
 
     async store(req, res) {
-
         const { author, place, description, hashtags } = req.body;
         const { filename: image } = req.file;
 
+        const [name] = image.split('.');
+        const fileName = `${name}.jpg`
+
+        let jimpImg = await jimp.read(req.file.path)
+        jimpImg.resize(500, 500)
+            .quality(70)
+            .write(
+                path.resolve(
+                    req.file.destination.replace(),
+                    'resized',
+                    fileName
+                )
+            );
+
+        fs.unlinkSync(req.file.path);
+
         const post = await Post.create({
-            author, place, description, hashtags, image
+            author, place, description, hashtags, fileName
         });
 
         res.json({ post });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path')
 
 mongoose.connect('mongodb+srv://semana:semana@mern-cluster-5vfir.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 
 app.use(require('./routes'))
 
